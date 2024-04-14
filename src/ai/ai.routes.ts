5d885007e1d62558eb48603f1b9587df4ef4a3c4
Pierre-Henry Soria <pierre@ph7.me>
2024-04-15 07:50:54 +1000

    fix import paths

--- src/ai/ai.routes.ts
+++ src/ai/ai.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { AIController } from './ai.controller';
-import { licensingMiddleware } from 'src/middlewares/licensing.middleware';
+import { licensingMiddleware } from '../middlewares/licensing.middleware';
 
 const router = express.Router();
 
